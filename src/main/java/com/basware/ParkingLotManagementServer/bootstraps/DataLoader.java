3be1f9d878527ea755204fbb4c9bbbad292b390c
BGheorghita <gheorghita.butnaru@basware.com>
2022-12-02 14:56:40 +0200

    Loading data at runtime

--- src/main/java/com/basware/ParkingLotManagementServer/bootstraps/DataLoader.java
+++ src/main/java/com/basware/ParkingLotManagementServer/bootstraps/DataLoader.java
@@ -7,10 +7,7 @@ import com.basware.ParkingLotManagementServer.models.parkings.spots.ParkingSpotT
 import com.basware.ParkingLotManagementServer.models.taxes.discounts.UserDiscount;
 import com.basware.ParkingLotManagementServer.models.users.UserType;
 import com.basware.ParkingLotManagementServer.models.vehicles.VehicleType;
-import com.basware.ParkingLotManagementServer.repositories.taxes.ParkingSpotPriceRepository;
-import com.basware.ParkingLotManagementServer.repositories.taxes.UserPriceRepository;
-import com.basware.ParkingLotManagementServer.repositories.taxes.VehiclePriceRepository;
-import com.basware.ParkingLotManagementServer.repositories.taxes.discounts.UserDiscountRepository;
+import com.basware.ParkingLotManagementServer.repositories.taxes.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -19,45 +16,48 @@ import org.springframework.stereotype.Component;
 @Component
 public class DataLoader implements CommandLineRunner {
     @Autowired
-    private ParkingSpotPriceRepository parkingSpotPriceRepository;
+    private ParkingSpotTypePriceDao parkingSpotTypePriceDao;
+
     @Autowired
-    private UserPriceRepository userPriceRepository;
+    private UserTypePriceDao userTypePriceDao;
+
     @Autowired
-    private VehiclePriceRepository vehiclePriceRepository;
+    private VehicleTypePriceDao vehicleTypePriceDao;
+
     @Autowired
-    private UserDiscountRepository userDiscountRepository;
+    private UserTypeDiscountDao userTypeDiscountDao;
 
     @Override
     public void run(String... args) {
-        parkingSpotPriceRepository.deleteAll();
-        userPriceRepository.deleteAll();
-        vehiclePriceRepository.deleteAll();
-        userDiscountRepository.deleteAll();
-        loadPricesForUsers();
+        parkingSpotTypePriceDao.deleteAll();
+        userTypePriceDao.deleteAll();
+        vehicleTypePriceDao.deleteAll();
+        userTypeDiscountDao.deleteAll();
         loadPricesForParkingSpots();
+        loadPricesForUsers();
         loadPricesForVehicles();
         loadUserDiscounts();
     }
 
     private void loadUserDiscounts() {
-        userDiscountRepository.save(new UserDiscount(UserType.REGULAR, new Price(0.25, Currency.EUR)));
-        userDiscountRepository.save(new UserDiscount(UserType.VIP, new Price(0.50, Currency.EUR)));
+        userTypeDiscountDao.save(new UserDiscount(UserType.REGULAR, new Price(0.25, Currency.EUR)));
+        userTypeDiscountDao.save(new UserDiscount(UserType.VIP, new Price(0.50, Currency.EUR)));
     }
 
     private void loadPricesForVehicles() {
-        vehiclePriceRepository.save(new VehiclePrice(VehicleType.MOTORCYCLE, new Price(0.5, Currency.EUR)));
-        vehiclePriceRepository.save(new VehiclePrice(VehicleType.CAR, new Price(0.15, Currency.EUR)));
-        vehiclePriceRepository.save(new VehiclePrice(VehicleType.TRUCK, new Price(0.20, Currency.EUR)));
+        vehicleTypePriceDao.save(new VehiclePrice(VehicleType.MOTORCYCLE, new Price(0.5, Currency.EUR)));
+        vehicleTypePriceDao.save(new VehiclePrice(VehicleType.CAR, new Price(0.15, Currency.EUR)));
+        vehicleTypePriceDao.save(new VehiclePrice(VehicleType.TRUCK, new Price(0.20, Currency.EUR)));
     }
 
     private void loadPricesForParkingSpots() {
-        parkingSpotPriceRepository.save(new ParkingSpotPrice(ParkingSpotType.SMALL, new Price(0.1, Currency.EUR)));
-        parkingSpotPriceRepository.save(new ParkingSpotPrice(ParkingSpotType.MEDIUM, new Price(0.2, Currency.EUR)));
-        parkingSpotPriceRepository.save(new ParkingSpotPrice(ParkingSpotType.LARGE, new Price(0.3, Currency.EUR)));
+        parkingSpotTypePriceDao.save(new ParkingSpotPrice(ParkingSpotType.SMALL, new Price(0.1, Currency.EUR)));
+        parkingSpotTypePriceDao.save(new ParkingSpotPrice(ParkingSpotType.MEDIUM, new Price(0.2, Currency.EUR)));
+        parkingSpotTypePriceDao.save(new ParkingSpotPrice(ParkingSpotType.LARGE, new Price(0.3, Currency.EUR)));
     }
 
     private void loadPricesForUsers() {
-        userPriceRepository.save(new UserPrice(UserType.VIP, new Price(0.5, Currency.EUR)));
-        userPriceRepository.save(new UserPrice(UserType.REGULAR, new Price(1.0, Currency.EUR)));
+        userTypePriceDao.save(new UserPrice(UserType.VIP, new Price(0.5, Currency.EUR)));
+        userTypePriceDao.save(new UserPrice(UserType.REGULAR, new Price(1.0, Currency.EUR)));
     }
 }
