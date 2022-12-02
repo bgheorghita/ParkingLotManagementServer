1e5ea132d7f3bbc5d6292505d4c2dc6dc8fb47c8
BGheorghita <gheorghita.butnaru@basware.com>
2022-12-02 14:52:12 +0200

    Getters & Setters & Constr

--- src/main/java/com/basware/ParkingLotManagementServer/models/taxes/ParkingSpotPrice.java
+++ src/main/java/com/basware/ParkingLotManagementServer/models/taxes/ParkingSpotPrice.java
@@ -1,14 +1,30 @@
 package com.basware.ParkingLotManagementServer.models.taxes;
 
 import com.basware.ParkingLotManagementServer.models.parkings.spots.ParkingSpotType;
-import lombok.*;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 public class ParkingSpotPrice {
     private ParkingSpotType parkingSpotType;
     private Price price;
+
+    public ParkingSpotPrice(ParkingSpotType parkingSpotType, Price price) {
+        this.parkingSpotType = parkingSpotType;
+        this.price = price;
+    }
+
+    public ParkingSpotType getParkingSpotType() {
+        return parkingSpotType;
+    }
+
+    public void setParkingSpotType(ParkingSpotType parkingSpotType) {
+        this.parkingSpotType = parkingSpotType;
+    }
+
+    public Price getPrice() {
+        return price;
+    }
+
+    public void setPrice(Price price) {
+        this.price = price;
+    }
 }
 
