3f8eacb05aab7baf9860660ba9bf2cdb059ea826
BGheorghita <gheorghita.butnaru@basware.com>
2022-12-02 14:57:56 +0200

    Removed unused imports

--- src/main/java/com/basware/ParkingLotManagementServer/ParkingLotManagementServerApplication.java
+++ src/main/java/com/basware/ParkingLotManagementServer/ParkingLotManagementServerApplication.java
@@ -1,17 +1,11 @@
 package com.basware.ParkingLotManagementServer;
 
-import com.mongodb.client.internal.MongoClientImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class ParkingLotManagementServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingLotManagementServerApplication.class, args);
 	}
-
 }
