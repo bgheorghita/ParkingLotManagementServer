698b0a39196cbb37e2441905669acc60ab88e42a
BGheorghita <gheorghita.butnaru@basware.com>
2022-12-03 19:26:20 +0200

    Added new exception handler

--- src/main/java/com/basware/ParkingLotManagementServer/controllers/taxes/APIExceptionHandlerController.java
+++ src/main/java/com/basware/ParkingLotManagementServer/controllers/taxes/APIExceptionHandlerController.java
@@ -1,6 +1,7 @@
 package com.basware.ParkingLotManagementServer.controllers.taxes;
 
 import com.basware.ParkingLotManagementServer.exceptions.ResourceNotFoundException;
+import com.basware.ParkingLotManagementServer.exceptions.ServiceNotAvailable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,5 +15,10 @@ public class APIExceptionHandlerController {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Object> handlerNotFoundException(Exception e, WebRequest request){
         return new ResponseEntity<Object>(RESOURCE_NOT_FOUND_MSG + e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ServiceNotAvailable.class)
+    public ResponseEntity<Object> handlerServiceNotAvailableException(Exception e, WebRequest request){
+        return new ResponseEntity<Object>(e.getMessage(), HttpStatus.SERVICE_UNAVAILABLE);
     }
 }
