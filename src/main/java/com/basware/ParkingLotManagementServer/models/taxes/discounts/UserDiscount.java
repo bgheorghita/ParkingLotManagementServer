86937ec2b308313a95a21003af5f5e4cae2e39b3
BGheorghita <gheorghita.butnaru@basware.com>
2022-12-03 19:27:14 +0200

    Small changes

--- src/main/java/com/basware/ParkingLotManagementServer/models/taxes/discounts/UserDiscount.java
+++ src/main/java/com/basware/ParkingLotManagementServer/models/taxes/discounts/UserDiscount.java
@@ -1,11 +1,21 @@
 package com.basware.ParkingLotManagementServer.models.taxes.discounts;
 
-import com.basware.ParkingLotManagementServer.models.taxes.Price;
-import com.basware.ParkingLotManagementServer.models.taxes.UserPrice;
 import com.basware.ParkingLotManagementServer.models.users.UserType;
 
-public class UserDiscount extends UserPrice {
-    public UserDiscount(UserType userType, Price price) {
-        super(userType, price);
+public class UserDiscount {
+    private double percent;
+    private UserType userType;
+
+    public UserDiscount(UserType userType, double percent) {
+        this.percent = percent;
+        this.userType = userType;
+    }
+
+    public double getPercent(){
+        return percent;
+    }
+
+    public UserType getUserType(){
+        return userType;
     }
 }
