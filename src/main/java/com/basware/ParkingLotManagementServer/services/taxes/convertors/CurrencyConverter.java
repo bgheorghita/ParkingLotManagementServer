544af4009c8d87823899d18d437f03f4f8f8b5f1
BGheorghita <gheorghita.butnaru@basware.com>
2022-12-03 19:25:59 +0200

    Signature changed

--- src/main/java/com/basware/ParkingLotManagementServer/services/taxes/convertors/CurrencyConverter.java
+++ src/main/java/com/basware/ParkingLotManagementServer/services/taxes/convertors/CurrencyConverter.java
@@ -1,10 +1,9 @@
 package com.basware.ParkingLotManagementServer.services.taxes.convertors;
 
+import com.basware.ParkingLotManagementServer.exceptions.ServiceNotAvailable;
 import com.basware.ParkingLotManagementServer.models.taxes.Currency;
 import com.basware.ParkingLotManagementServer.models.taxes.Price;
 
-import java.util.Optional;
-
 public interface CurrencyConverter {
-    Optional<Price> convert(Currency fromCurrency, Currency toCurrency, double amount);
+    Price convert(Currency fromCurrency, Currency toCurrency, double amount) throws ServiceNotAvailable;
 }
