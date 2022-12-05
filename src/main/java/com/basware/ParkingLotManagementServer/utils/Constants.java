d72eb323e82004e4ab7646303e1691970aab795a
BGheorghita <gheorghita.butnaru@basware.com>
2022-12-05 13:00:43 +0200

    Added default user discount constant

--- src/main/java/com/basware/ParkingLotManagementServer/utils/Constants.java
+++ src/main/java/com/basware/ParkingLotManagementServer/utils/Constants.java
@@ -2,4 +2,5 @@ package com.basware.ParkingLotManagementServer.utils;
 
 public class Constants {
     public static int DISCOUNT_AVAILABLE_AFTER_MINUTES = 180;
+    public static double DEFAULT_USER_DISCOUNT_PERCENT = 0;
 }
